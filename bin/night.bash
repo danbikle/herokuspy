a4b4e0b0ea149124d1ffb308167a4308380e5d40
Dan Bikle <dan.bikle@gmail.com>
2015-12-31 19:41:05 +0000

    more

--- bin/night.bash
+++ bin/night.bash
@@ -3,8 +3,8 @@
 # night.bash
 . ${HOME}/pyspy/pyspy_env.bash
 . ${HOME}/herokuspy/herokuspy_env.bash
-  ${HOME}/pyspy/bin/night.bash
-cp ${DDATA}learn_test.png $STATICF
+${PYSPY}/bin/night.bash
+cp ${DDATA}/learn_test.png $STATICF
 # I should publish the changes
 cd $HEROKUSPY
 git commit -am nightran
--- bin/noon50.bash
+++ bin/noon50.bash
@@ -3,8 +3,8 @@
 # noon50.bash
 . ${HOME}/pyspy/pyspy_env.bash
 . ${HOME}/herokuspy/herokuspy_env.bash
-  ${HOME}/pyspy/bin/noon50.bash
-cp ${DDATA}learn_test.png $STATICF
+${PYSPY}/bin/noon50.bash
+cp ${DDATA}/learn_test.png $STATICF
 # I should publish the changes
 cd $HEROKUSPY
 git commit -am noon50ran
