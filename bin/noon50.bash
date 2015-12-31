a4a04952e38a4673a0da10007b1285d0613b617e
Dan Bikle <dan.bikle@gmail.com>
2015-12-31 20:13:12 +0000

    more

--- bin/noon50.bash
+++ bin/noon50.bash
@@ -4,7 +4,8 @@
 . ${HOME}/pyspy/pyspy_env.bash
 . ${HOME}/herokuspy/herokuspy_env.bash
 ${PYSPY}/bin/noon50.bash
-cp ${DDATA}/learn_test.png $STATICF
+cp  ${DDATA}/learn_test.png $STATICF
+cat ${DDATA}/learn_test.csv|awk -F, '{print $1","$2","$10","$11}' |sed '1,$s/1.000$/1/' > ${STATICF}/predictions.csv
 # I should publish the changes
 cd $HEROKUSPY
 git commit -am noon50ran
