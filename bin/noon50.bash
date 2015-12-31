682309ff601f75893fc00a0ca7fcdd08d1499c28
Dan Bikle <dan.bikle@gmail.com>
2015-12-31 22:16:36 +0000

    more

--- bin/noon50.bash
+++ bin/noon50.bash
@@ -6,6 +6,17 @@
 ${PYSPY}/bin/noon50.bash
 cp  ${DDATA}/learn_test.png $STATICF
 cat ${DDATA}/learn_test.csv|awk -F, '{print $1","$2","$10","$11}' |sed '1,$s/1.000$/1/' > ${STATICF}/predictions.csv
+
+# I should add some html
+cd ${HEROKUSPY}/hello/templates
+echo '<pre class="pcsv"><code>' > predictions.html
+echo '<h1>Predictions</h1>'    >> predictions.html
+cat ${STATICF}/predictions.csv >> predictions.html
+echo '</code></pre>'           >> predictions.html
+exit
+
+cp base_empty.html                           base.html
+sed -i '/predictionsHere/r predictions.html' base.html
 # I should publish the changes
 cd $HEROKUSPY
 git commit -am noon50ran
